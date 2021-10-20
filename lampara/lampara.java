c53b8048a5fac43f7ea0ab5e98a7f78a01374706
Alberto Basalo <albertobasalo@hotmail.com>
2021-10-20 18:24:50 +0200

    Clone from https://bitbucket.org/curso-git-avanzado/unidad-4-ejercicio-2/src/master/

---
+++ lampara/lampara.java
@@ -0,0 +1,12 @@
+class Lampara {
+
+  private boolean estaEncendida;
+
+  public void enciende() {
+    estaEncendida = true;
+  }
+
+  public void apaga() {
+  	estaEncendida = false;
+  }
+}
---
+++ lavadora/lavadora.java
@@ -0,0 +1,12 @@
+class Lavadora {
+
+  private int programa;
+  private boolean aguaFria;
+  private boolean centrifugado;
+  private boolean enMarcha;
+
+  void lava() {
+    enMarcha = true;
+  }
+
+}
